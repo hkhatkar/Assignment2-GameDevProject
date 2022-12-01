881f75e6de2bb8c379e8ad36050ca19fb28e20a7
Jordan D'Souza <jordan.dsouza@ntlworld.com>
2022-12-01 16:05:20 +0000

    Update UIController.cs
    loadcred and loadmenu functions added for further navigation to and from the main menu

--- Assign2_GamedevProject/Assets/Scripts/UIController.cs
+++ Assign2_GamedevProject/Assets/Scripts/UIController.cs
@@ -9,4 +9,12 @@ public class UIController : MonoBehaviour
     public void loadgame(){
         SceneManager.LoadScene("mainGame");
     }
+
+    public void loadcred(){
+        SceneManager.LoadScene("creditScreen");
+    }
+
+    public void loadmenu(){
+        SceneManager.LoadScene("mainMenu");
+    }
 }
